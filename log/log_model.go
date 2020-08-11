332a615ef232f6478fc9a4ff5f6179dd561e94da
吴文凯 <accounts_5e3fa2019cbc2b0021d890f6@mail.teambition.com>
2020-08-11 17:28:18 +0800

    update log

--- log/log_model.go
+++ log/log_model.go
@@ -9,9 +9,18 @@ type Log struct {
 func (l *Log)Infow(msg string, keysAndValues ...interface{})  {
 	l.log.Infow(msg,keysAndValues...)
 }
-func (l *Log)Warnw(msg string, keysAndValues ...interface{})  {
-	l.log.Warnw(msg,keysAndValues...)
+func (l *Log)Info(args ...interface{})  {
+	l.log.Info(args...)
+}
+func (l *Log)Errorw(msg string, keysAndValues ...interface{})  {
+	l.log.Errorw(msg,keysAndValues...)
+}
+func (l *Log)Error(args ...interface{})  {
+	l.log.Error(args...)
 }
 func (l *Log)Debugw(msg string, keysAndValues ...interface{})  {
 	l.log.Debugw(msg,keysAndValues...)
+}
+func (l *Log)Debug(args ...interface{})  {
+	l.log.Debug(args...)
 }
