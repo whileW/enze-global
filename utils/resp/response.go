ad742fff068ba8844cdb57a0dcfe883fc552af37
吴文凯 <accounts_5e3fa2019cbc2b0021d890f6@mail.teambition.com>
2020-08-14 09:52:16 +0800

    move page to response

--- utils/resp/response.go
+++ utils/resp/response.go
@@ -11,6 +11,13 @@ type Response struct {
 	Msg  	string      	`json:"msg"`
 }
 
+type PageResult struct {
+	List     interface{} `json:"list"`
+	Total    int         `json:"total"`
+	Page     int         `json:"page"`
+	PageSize int         `json:"pageSize"`
+}
+
 type ResCode int
 const (
 	ERROR   		ResCode			= 	500
