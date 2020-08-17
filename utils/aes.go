358d8ab6ed951bb851d5b14f1458d72fe8557763
吴文凯 <accounts_5e3fa2019cbc2b0021d890f6@mail.teambition.com>
2020-08-17 17:32:07 +0800

    add aes decrypt

---
+++ utils/aes.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+)
+
+func aes_decrypt(Input string,Iv,Key []byte) ([]byte,error) {
+	block, err := aes.NewCipher(Key)
+	if err != nil {
+		return nil,err
+	}
+	msg,_ := base64.StdEncoding.DecodeString(Input)
+	cipherText := make([]byte, len(msg))
+	mode := cipher.NewCBCDecrypter(block, Iv)
+	mode.CryptBlocks(cipherText,msg)
+	return decode2(cipherText),nil
+}
+func  decode2(decrypted []byte)[]byte {
+	pad := int(decrypted[len(decrypted)-1])
+	if pad < 1 || pad > 32 {
+		pad = 0
+	}
+	res := decrypted[:len(decrypted)-pad]
+	return res
+}
