03a8128c8979816ed5379884b3464b23d9a2b74e
吴文凯 <accounts_5e3fa2019cbc2b0021d890f6@mail.teambition.com>
2020-09-23 15:16:09 +0800

    update response

--- utils/resp/response.go
+++ utils/resp/response.go
@@ -8,7 +8,7 @@ import (
 type Response struct {
 	Code 	ResCode         `json:"code"`
 	Data 	interface{} 	`json:"data"`
-	Msg  	string      	`json:"msg"`
+	Msg  	interface{}    `json:"msg"`
 }
 
 type BasePageModel struct {
@@ -30,7 +30,7 @@ const (
 	Unauthorized	ResCode			=	402
 )
 
-func Result(c *gin.Context,ResCode ResCode,ResMsg string,ResData interface{})  {
+func Result(c *gin.Context,ResCode ResCode,ResMsg interface{},ResData interface{})  {
 	c.JSON(http.StatusOK,Response{
 		Code:ResCode,
 		Msg:ResMsg,
@@ -45,13 +45,13 @@ func NoFindResult(c *gin.Context)  {
 func Ok(c *gin.Context)  {
 	Result(c,SUCCESS,"操作成功",nil)
 }
-func OkWithMessage(c *gin.Context,ResMsg string) {
+func OkWithMessage(c *gin.Context,ResMsg interface{}) {
 	Result(c,SUCCESS, ResMsg, nil)
 }
 func OkWithData(c *gin.Context,ResData interface{}) {
 	Result(c,SUCCESS, "操作成功", ResData)
 }
-func OkDetailed(c *gin.Context,ResMsg string,ResData interface{}) {
+func OkDetailed(c *gin.Context,ResMsg interface{},ResData interface{}) {
 	Result(c,SUCCESS,ResMsg, ResData)
 }
 
@@ -59,12 +59,12 @@ func Fail(c *gin.Context) {
 	Result(c,ERROR,"操作失败",nil)
 }
 
-func FailWithMessage(c *gin.Context,ResMsg string) {
+func FailWithMessage(c *gin.Context,ResMsg interface{}) {
 	Result(c,ERROR, ResMsg,nil)
 }
 func FailWithData(c *gin.Context,ResData interface{}) {
 	Result(c,ERROR, "操作失败",ResData)
 }
-func FailWithDetailed(c *gin.Context,ResCode ResCode, ResMsg string, ResData interface{}) {
+func FailWithDetailed(c *gin.Context,ResCode ResCode, ResMsg interface{}, ResData interface{}) {
 	Result(c,ResCode, ResMsg, ResData)
 }
