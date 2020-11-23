48577551bcd219dac72cb90e393072320a14c779
wuwk@geeran.cn <wuwk@geeran.cn>
2020-11-23 10:37:26 +0800

    update etcd

--- runtest/etcd.go
+++ runtest/etcd.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/whileW/enze-global"
+	"github.com/whileW/enze-global/initialize"
 )
 
 func main()  {
-	//initialize.Etcd()
+	initialize.Etcd()
 	//fmt.Println(global.GVA_ETCD.Put("/enze/test1","127.0.0.1"))
 	//fmt.Println(global.GVA_ETCD.Put("/enze/test2","127.0.0.2"))
 	//global.GVA_ETCD.Delete("/enze/test2")
+	initialize.RegisterByEtcdHTTP("test")
 	fmt.Println(global.GVA_ETCD.GetWithPrefix(""))
 }
