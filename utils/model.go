2e363826e7b01c96930c6f72a12061c7a45478ac
wuwk@geeran.cn <wuwk@geeran.cn>
2020-11-25 15:37:07 +0800

    update basemode

--- utils/model.go
+++ utils/model.go
@@ -3,9 +3,10 @@ package utils
 import "time"
 
 type BaseModel struct {
+	ID 			int			`gorm:"primary_key"`
 	CreatedAt time.Time	`json:"created_at" gorm:"type:datetime"`
 	UpdatedAt time.Time	`json:"updated_at" gorm:"type:datetime"`
-	DeletedAt *time.Time	`json:"deleted_at" gorm:"type:datetime"`
+	DeletedAt *time.Time	`json:"deleted_at" sql:"index"`
 }
 
 
